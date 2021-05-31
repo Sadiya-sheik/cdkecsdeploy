6d5dc04ecd239c93e362e1dbe310c268c50055be
Sadiya-sheik <83219444+Sadiya-sheik@users.noreply.github.com>
2021-05-31 20:21:34 +0530

    Update acertus-ecs-cdk-stack.ts

--- lib/acertus-ecs-cdk-stack.ts
+++ lib/acertus-ecs-cdk-stack.ts
@@ -17,7 +17,7 @@ export class AcertusEcsCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const routecertificate = certificatemanager.Certificate.fromCertificateArn(this, "certificate", "	arn:aws:acm:ap-south-1:665106695518:certificate/d55e09cc-f9e7-4b8e-9c69-0d85bcd24436");
+    const routecertificate = certificatemanager.Certificate.fromCertificateArn(this, "certificate", "arn:aws:acm:ap-south-1:665106695518:certificate/d55e09cc-f9e7-4b8e-9c69-0d85bcd24436");
     //const cloudfronturl = "arn:aws:cloudfront::850805969385:distribution/E3SXCGGDWS0B0P";
     const clientPrefix = "ADMINUI";
     // New VPC creation
